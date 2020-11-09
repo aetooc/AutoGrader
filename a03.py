99c2d53d3ecbe21b7f804a4fbd0dfc1a84597f39
Muhammad Awais <72553466+aetooc@users.noreply.github.com>
2020-11-09 22:57:32 +0500

    a03.py
    a03.py

---
+++ a03.py
@@ -0,0 +1,38 @@
+### YOUR CODE FOR chocolatePrice() FUNCTION GOES HERE ###
+def chocolatePrice(ali_budget, bashir_budget):
+    
+    if ali_budget >=0 and bashir_budget >=0:
+        for i in range(1,ali_budget+1 and bashir_budget+1):
+            if ali_budget%i==bashir_budget%i==0:
+                price = i
+        return(price)
+    else:
+        return 'Not Possible'
+
+#### End OF MARKER
+
+
+
+
+
+### YOUR CODE FOR calculateProfit() FUNCTION GOES HERE ###
+def calculateProfit(ali_budget, bashir_budget):
+    
+    if type(ali_budget)== str or type(bashir_budget)==str:
+        return 'Not Possible'
+    
+    if ali_budget ==0 or bashir_budget==0 or ali_budget < 0 or bashir_budget < 0 :
+        return "Not Possible"
+   
+    if ali_budget > bashir_budget:
+        A=(bashir_budget*2) - bashir_budget
+        B=(ali_budget*(1.5)) - ali_budget
+    else:
+        A=(ali_budget*2) - ali_budget
+        B=(bashir_budget*(1.5)) - bashir_budget
+        
+    if A > B:
+        return (int(A))
+    else:
+        return (int(B))       
+#### End OF MARKER
